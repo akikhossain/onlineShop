3d895541f649d25539f46feff8a780c61bb90abc
akik-hossain <akikhs00@gmail.com>
2024-05-31 00:41:55 +0600

    deleted temporary and thumb file or image from the server

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\TempImage;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 
 class HomeController extends Controller
 {
@@ -44,6 +46,35 @@ class HomeController extends Controller
             ->whereDate('created_at', '<=', $currentDate)
             ->sum('grand_total');
 
+
+        // delete temp images
+        $dayBeforeToday = Carbon::now()->subDays(1)->format('Y-m-d H:i:s');
+        // dd($dayBeforeToday);
+        $tempImages = TempImage::where('created_at', '<=', $dayBeforeToday)->get();
+
+        foreach ($tempImages as $tempImage) {
+            // echo $tempImage->created_at . '===' . $tempImage->id;
+            // echo '<br>';
+
+            $path = public_path('temp/' . $tempImage->image);
+            $thumbPath = public_path('/temp/thumb/' . $tempImage->image);
+
+            // echo $path . '<br>';
+            // echo $thumbPath . '<br>';
+
+            // path delete
+            if (File::exists($path)) {
+                File::delete($path);
+            }
+
+            // thumb path delete
+            if (File::exists($thumbPath)) {
+                File::delete($thumbPath);
+            }
+
+            // delete from database
+            TempImage::where('id', $tempImage->id)->delete();
+        }
 
         return view('Admin.dashboard', compact(
             'totalOrders',
